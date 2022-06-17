bcb02ea1af2ce344905f6d8724ca33c45587c579
ysncastro <yasmincastrosza@gmail.com>
2022-06-17 20:31:39 -0300

    criação classe Livro e classe Autor

---
+++ src/main/java/com/yasmin/biblioteca/domain/Autor.java
@@ -0,0 +1,12 @@
+package com.yasmin.biblioteca.domain;
+
+import java.util.List;
+
+public class Autor {
+
+    private Long id;
+    private String nome;
+    private List<Livro> publicacoes;
+
+
+}
---
+++ src/main/java/com/yasmin/biblioteca/domain/Livro.java
@@ -0,0 +1,9 @@
+package com.yasmin.biblioteca.domain;
+
+public class Livro {
+
+    private Long id;
+    private String isbn;
+    private Autor autor;
+
+}

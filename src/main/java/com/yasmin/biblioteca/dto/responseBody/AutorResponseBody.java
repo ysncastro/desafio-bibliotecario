0f44c255452da32a6d993408c7f2e98f09d25c9a
ysncastro <yasmincastrosza@gmail.com>
2022-06-20 17:51:23 -0300

    Implementação Mapper, criação service

--- src/main/java/com/yasmin/biblioteca/dto/responseBody/AutorResponseBody.java
+++ src/main/java/com/yasmin/biblioteca/dto/responseBody/AutorResponseBody.java
@@ -1,10 +1,7 @@
 package com.yasmin.biblioteca.dto.responseBody;
 
 import com.yasmin.biblioteca.domain.Livro;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
@@ -12,6 +9,7 @@ import java.util.List;
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class AutorResponseBody {
 
     private Long id;
